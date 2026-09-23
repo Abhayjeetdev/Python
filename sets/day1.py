4ae127b099fcdea5edfc52cfc39dc4989e358977
Abhayjeet Kumar <abhayjeetkumar084@gmail.com>
2026-09-23 21:10:11 +0530

    i have learned about set and tuples

---
+++ sets/day1.py
@@ -0,0 +1,47 @@
+# set ={1,1,1,1,2,2,2,3,3,3,4,4,5,5}
+# print(set)
+# for i in set:
+#     print(i)
+
+# set = {1,1,7,8,9,2,3,3,7,8,6,5,6,3,4,4}
+# #add
+# set.add("abhay")
+# #remove
+# set.remove(6)
+# #discard
+# set.discard(10)
+# print(set)
+
+# set1={10,20,10,30,20,40,50}
+# sum = sum(set1)
+# print(sum)
+
+# marks ={90,85,75,93,46}
+# sum = sum(marks)
+# length = len(marks)
+# average = sum/length
+# print(average)
+
+# num = int(input("enter any number:"))
+# sum = 0
+# while num!=0:
+#     sum = sum+num
+#     num = int(input("enter any number:"))
+# print(sum)
+
+#union
+A={1,2,3,4}
+B={3,4,5,6}
+# print(A|B)
+# print(A.union(B))
+
+#Intersection
+# print(A&B)
+# print(A.intersection(B))
+
+#Difference
+# print(A-B)
+# print(A.difference(B))
+
+# print(B-A)
+# print(B.difference(A))
---
+++ tuples/day1.py
@@ -0,0 +1,16 @@
+# marks = (10,20,30,40,50)
+# sum = 0
+# for mark in marks :
+#     sum = sum + mark
+# print("marks:",marks)
+# print("sum of marks:",sum)
+
+# Tuple Unpacking
+# user_profile = ("Abhayjeet", "abhayjeet.kumar.581", 491, 492, 0)
+# name,user_name,followers,following,post=user_profile
+# print(user_profile)
+# print("name:",name)
+# print("user_name:",user_name)
+# print("followers:",followers)
+# print("following:", following)
+# print("posts:",post)
